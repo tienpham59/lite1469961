42a218ed5a27f71c17411e3016a8acda64330fa1
tienpham59 <pht.phamtien@gmail.com>
2024-06-13 18:45:20 +0700

    Update start.js

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const { query } = require("./data.js");
 
 app.use(express.json());
 
-app.get("/api/books", async (req, res) => {
+app.get("/api1469961/books", async (req, res) => {
   console.info(`Getting all books`);
 
   const books = await query("SELECT * FROM tblbook");
